69fac66b7da5802201c71d53edc2a16e0d4946b8
Штирлиц <152861800+Dfhjtssg@users.noreply.github.com>
2025-01-12 14:02:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Обработчик кликов
 function handleButtonClick(buttonName) {
     console.log(`Кнопка "${buttonName}" нажата`);
-    alert(`Кнопка "${buttonName}" нажата`);
+    // alert(`Кнопка "${buttonName}" нажата`);
 }
